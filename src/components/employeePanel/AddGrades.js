d7971a44f8547550ef42bc9456d008c4649876ad
RADq1 <1998radq@gmail.com>
2022-10-10 01:10:07 +0200

    add grades

--- src/components/employeePanel/AddGrades.js
+++ src/components/employeePanel/AddGrades.js
@@ -1,22 +1,54 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
 import { useSelector, useDispatch} from "react-redux";
-// import { useForm } from "react-hook-form";
 import axios from "axios";
-import { setIn } from "formik";
+
+import { SET_ERROR, SET_GRADES, SET_NEW_GRADE } from "../../shared/constants/gradeReducerAction";
+
+const gradeReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_GRADES':
+      return {
+        ...state,
+        grades: action.grades
+      }
+    case 'SET_ERROR':
+      return {
+        ...state,
+        loading: false,
+        grades: null,
+        error: action.error
+      }
+    case 'SET_NEW_GRADE':
+      return {
+        ...state, //Tutaj mamy sobie stan w reducerze z flagami loading, error no i tablica grades (... spread operator powoduje, ze te flagi jak loading, error nie zostana utracone wraz ze zmianami. Jedyne co robimy to modyfikujemy tablice grades, taki jest cel)
+        grades: state.grades.map((grade) => grade.id === action.payload.id //Tutaj mamy tablice grades, mapujemy sobie po wszystkich obiektach sprawdzając czy dany obiekt z ocena ma takie samo id jak przekażemy w dispatch, czyli pozwoli to odróżnić jaki input no bo każdy input ma swoje data.id a więc to data.id będzie przekazane w payload i no pozwoli nam to zrobić. :)
+        ? {...grade, grade: action.payload.grade ? action.payload.grade : null} //A tutaj mamy warunek trójargumentowy po polsku mówiąc, czyli znowu mamy {...grade} co oznacza, że jak mamy w obiekcie grade więcej pól niż tylko grade np. null albo ten obiekt users, etc. No to też nie chcemy tego przy return stracić a zmodyfikować jedynie grade. Więc skoro nie chcemy stracić, używamy spread operatora czyli pozostaw przy zwracaniu wszystkie pozostałe właściwości obiektu i zmodyfikuj tylko tą wskazaną. :)
+        : grade //No a w przypadku kiedy grade.id !== od action.payload.id no to wtedy nie chcemy zmieniać obiektu no bo po co. Więc zwracamy go w czystej formie w takiej jak był wcześniej już w state reducera w tablicy grades.
+        )
+      };
+    default:
+    return state;
+  }
+}
+
 const AddGrades = ({ setNumber }) => {
     const { user: currentUser } = useSelector((state) => state.auth);
     const [data, setData] = useState(null);
-    const [grades, setGrades] = useState(null);
+    // const [grades, setGrades] = useState(null);
+    const [state, dispatch] = useReducer(gradeReducer, {
+      loading: true,
+      grades: null,
+      error: null
+    });
     const [value, setValue] = useState(null);
     const [inputValue, setInputValue] = useState(null);
-    // const [gradeInput, setGradeInput] = useState([]);
-    // const {handleSubmit, register, setValue: setValues, formState: { errors },} = useForm();
-    // const currentListGrades = data?.filter((grade) => {
-    //     return grade.Lesson.name === value;
-    // })
+
+    console.log('stateGrade', state);
+
     const change = (e) => {
         setValue(e.target.value);
     }
+
     useEffect(() => {
         const baseUrl = "http://localhost:8080";
         const url = baseUrl + "/showGrade/" + value
@@ -24,20 +56,22 @@ const AddGrades = ({ setNumber }) => {
           .get(url)
           .then((res) => {
             if (res.data.success) {
-              const grades = res.data.data;
-              console.log(grades);
-              setGrades(grades);
-              if (!grades) return;
+              const data = res.data.data;
+              // setGrades(grades);
+              dispatch({ type: SET_GRADES, grades: res.data.data })
+              if (!data) return;
             } else {
               alert("Bład");
             }
           })
           .catch((error) => {
             alert("Błąd po stronie serwera" + error);
+            dispatch({ type: SET_ERROR, error: 'Problem z pobraniem listy ocen oraz użytkowników'});
           });
         // eslint-disable-next-line
       }, [value]);
-    useEffect(() => {
+
+      useEffect(() => {
         const baseUrl = "http://localhost:8080";
         const url = baseUrl + "/showAssignedLessons/" + currentUser.id;
         axios
@@ -57,79 +91,28 @@ const AddGrades = ({ setNumber }) => {
           });
         // eslint-disable-next-line
       }, []);
-    const sendGrades = (e) => {
+    
+      const sendGrades = (e) => {
         e.preventDefault()
-        console.log(value)
-        console.log(grades)
-        console.log(inputValue);
         const baseUrl = "http://localhost:8080/addGrades";
           try {
             axios
               .post(baseUrl, {
-                grades: grades,
+                grades: state.grades,
               })
               .then((res) => console.log(res));
           } catch (e) {
             console.log(e);
           }
-    }
-    const changeGrade = (e, id) => {
-      const filterGrades = grades.map((item) => {
-        // console.log("grade id: ", grade.id)
-        if(item.id === id)
-        {
-          const xd = {...item, grade: e.target.value}
-          console.log(xd);
-          return xd;
-        } else {
-          console.log("nie")
-          return item;
         }
-      })
-      // const newGrades = grades.map((item, index) => {
-      //    return {...item, grade: e.target.value}
-      // })
-    }
 
-    const handleChangeInput = (e, index, id) => {
-      // setGrades(newArray);
-      let newArray;
-      newArray = [...grades];
-      grades.forEach((grade) => {
-        if(grade.id === id)
-        {
-          setGrades(
-            grades.map(item =>
-                item.id === index
-                ? {...item, grade : e.target.value}
-                : item
-          ))
-          newArray[index] = {...grade, grade: e.target.value};
-          console.log("index z if",index);
-        }
-        // else{
-        //   newArray[index] = grade;
-        //   console.log("index z else",index);
-        // }
-      })
-      console.log("newArray", newArray);
-      // setGrades(newArray);
- 
-      // console.log(newInputFields)
-      // setGrades(newArray)
-      // setInputValue(newInputFields);
-      // setGrades((prevState) => (
-      //   [...prevState, newInputFields]
-      // ));
-      // console.log(newInputFields);
-    }
         return(
         <div>
             <p>Wybierz przedmiot</p>
-            <form onSubmit={sendGrades}>
+            <form onSubmit={() => {}}>
             <select onChange={change}>
             {data?.map((data) => {
-            return <option value={data.id}>{data.name}, semestr: {data.semestr}</option>
+            return <option key={data.id} value={data.id}>{data.name}, semestr: {data.semestr}</option>
             })}
             </select>
             <table>
@@ -138,21 +121,18 @@ const AddGrades = ({ setNumber }) => {
                     <th>Nazwisko</th>
                     <th>Ocena</th>
                 </tr>
-            {grades?.map((data, index) => {
-                return  <tr>
-                <td>{data.user.name}</td>
-                <td>{data.user.surname}</td>
+                {state.grades?.map(({user, id, grade}) => {
+                return <tr>
+                <td>{user.name}</td>
+                <td>{user.surname}</td>
                 {/* step="0.5" */}
-                <input key={data.id} type="number" min="2" max="5" onChange={(e) => handleChangeInput(e, index, data.id)}>{data.grade}</input>
+                <input key={id} type="number" min="2" max="5" onChange={(event) => {
+                  dispatch({ type: SET_NEW_GRADE, payload: { id, grade: event.target.value }})
+                }}/>
                 </tr>
             })}
             </table>
-            {/* {currentListGrades?.map((grade) => {
-                    return <div>
-                        <input type="number"/>
-                    </div>
-                })} */}
-            <button type="submit" onClick={() => {}}>Dodaj oceny</button>
+            <button type="submit" onClick={sendGrades}>Dodaj oceny</button>
             </form>
             <button onClick={() => setNumber(2)}>Wróć</button>
         </div>
---
+++ src/shared/constants/gradeReducerAction.js
@@ -0,0 +1,3 @@
+export const SET_GRADES = "SET_GRADES";
+export const SET_ERROR = 'SET_ERROR';
+export const SET_NEW_GRADE = "SET_NEW_GRADE";
